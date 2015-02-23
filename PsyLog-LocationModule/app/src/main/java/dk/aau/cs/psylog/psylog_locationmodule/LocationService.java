5aa375f8200d9eabc78c2738ef9a352f2f6ca9c1
Bruno Thalmann <mr.thalmann@gmail.com>
2015-02-23 16:11:44 +0100

    initial

---
+++ PsyLog-LocationModule/app/src/main/java/dk/aau/cs/psylog/psylog_locationmodule/LocationService.java
@@ -0,0 +1,16 @@
+package dk.aau.cs.psylog.psylog_locationmodule;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocationService extends Service {
+    public LocationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
