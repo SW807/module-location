c70c6bb513073e32e4c29be1e31be58128f3ebe8
Mathias Winde <Mapede11@student.aau.dk>
2015-03-03 09:56:41 +0100

    Skulle lige fjerne androids service

--- app/src/main/java/dk/aau/cs/psylog/psylog_locationmodule/Service.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_locationmodule/Service.java
@@ -1,9 +1,5 @@
 package dk.aau.cs.psylog.psylog_locationmodule;
 
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.os.IBinder;
 import dk.aau.cs.psylog.module_lib.SuperService;
 
 
