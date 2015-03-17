9293f5c7418a493105c90afa29ae5eb91a470366
Mathias Winde <Mapede11@student.aau.dk>
2015-03-17 11:34:24 +0100

    Opdatered service så den bruger dne nye service

--- app/src/main/java/dk/aau/cs/psylog/location/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/location/PsyLogService.java
@@ -1,9 +1,9 @@
 package dk.aau.cs.psylog.location;
 
-import dk.aau.cs.psylog.module_lib.SuperService;
+import dk.aau.cs.psylog.module_lib.SensorService;
 
 
-public class PsyLogService extends SuperService {
+public class PsyLogService extends SensorService {
     @Override
     public void setSensor() {
         sensor = new LocationSensorListener(this,1000,0);
